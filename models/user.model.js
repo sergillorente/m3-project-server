9562f469f95995e720f391fa6a3d5f4dce040737
[sergillorente] <[sergi.llorente.blasco@gmail.com]>
2020-12-12 00:20:13 +0100

    Predefault profile picture set in the hotel model

--- models/user.model.js
+++ models/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new Schema(
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, unique: true },
-    picture: String
+    picture: { type: String, default: '/images/default-profile.jpg' }
   }
 );
 
